9f068c19d962744d78b1a9b63f53c90aa95ffbf2
JOAQUIN LEONELLI <jleonelli@ar0fvfwm184hv2j.ml.com>
2019-07-16 17:03:35 -0300

    Se agrega localStorage y loading

--- src/Home.js
+++ src/Home.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import './App.css';
 import NavigationBar from './components/NavigationBar';
 import Article from "./components/Article";
+import ReactLoading from 'react-loading';
 
 class Home extends Component {
   state = {
@@ -9,18 +10,34 @@ class Home extends Component {
 }
 
 componentDidMount() {
-    var url = 'https://newsapi.org/v2/top-headlines?' +
-    'country=us&' +
-    'apiKey=9c08cbd8ac004bb08d92315c5bfb5fe6';
-    var req = new Request(url);
-    fetch(req).then(response => {
-      response.json().then(resp => {
-          const a = resp
-          this.setState({
-              headlines: a.articles
-          });
-     });
-  });
+    const localStorageRef = localStorage.getItem("headlines");
+    if (localStorageRef) {
+        setTimeout(() => {
+            this.setState({
+                headlines: JSON.parse(localStorageRef)
+            });
+        }, 1000);
+    } else {
+        var url = 'https://newsapi.org/v2/top-headlines?' +
+        'country=us&' +
+        'apiKey=9c08cbd8ac004bb08d92315c5bfb5fe6';
+        var req = new Request(url);
+        fetch(req).then(response => {
+          response.json().then(resp => {
+              const a = resp
+              this.setState({
+                  headlines: a.articles
+              });
+         });
+      });
+    }
+}
+
+componentDidUpdate() {
+    localStorage.setItem(
+        "headlines", 
+        JSON.stringify(this.state.headlines)
+    );
 }
 
 handleClick(e) {
@@ -41,8 +58,14 @@ render() {
         <React.Fragment>
             <NavigationBar clickHandler={this.handleClick}/>
             <h1 className="headlines-title">Headlines</h1>
-            { this.state.headlines.length > 0 &&
-               this.renderHeadlines() }
+            { this.state.headlines.length > 0 ?
+                this.renderHeadlines() : 
+                <div className="centered-loading">
+                    <ReactLoading type="spin" color={'#282c34'} height={75} width={75} />
+                </div>
+            }
+
+
         </React.Fragment>
     );
   }
--- src/components/Category.js
+++ src/components/Category.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Article from "./Article";
 import NavigationBar from "./NavigationBar";
+import ReactLoading from 'react-loading';
 
 class Category extends React.Component {
     state = {
@@ -8,15 +9,31 @@ class Category extends React.Component {
     }
 
     componentDidMount() {
-        var url = `https://newsapi.org/v2/top-headlines?country=us&category=${this.getCategory()}&apiKey=9c08cbd8ac004bb08d92315c5bfb5fe6`;
-        var req = new Request(url);
-        fetch(req).then(response => {
-            response.json().then(resp => {
+        const localStorageRef = localStorage.getItem(this.getCategory());
+        if (localStorageRef) {
+            setTimeout(() => {
                 this.setState({
-                    articles: resp.articles
+                    articles: JSON.parse(localStorageRef)
+                });
+            }, 1000);
+        } else {
+            var url = `https://newsapi.org/v2/top-headlines?country=us&category=${this.getCategory()}&apiKey=9c08cbd8ac004bb08d92315c5bfb5fe6`;
+            var req = new Request(url);
+            fetch(req).then(response => {
+                response.json().then(resp => {
+                    this.setState({
+                        articles: resp.articles
+                    });
                 });
             });
-        });
+        }
+    }
+
+    componentDidUpdate() {
+        localStorage.setItem(
+            this.getCategory(), 
+            JSON.stringify(this.state.articles)
+        );
     }
 
     handleClick(e) {
@@ -45,7 +62,11 @@ class Category extends React.Component {
             <div>
                 <NavigationBar clickHandler={this.handleClick}/>
                 <h1 className="category-title">{this.capitalize(this.getCategory())}</h1>
-                { this.state.articles.length > 0 && this.renderArticles() }
+                { this.state.articles.length > 0 ? 
+                    this.renderArticles() :
+                    <div className="centered-loading">
+                    <ReactLoading type="spin" color={'#282c34'} height={75} width={75} />
+                </div>}
             </div>
 
         )
