ce0f0204bb2abf0c1bbbb8da923205d581aedad0
JOAQUIN LEONELLI <jleonelli@ar0fvfwm184hv2j.ml.com>
2019-07-23 00:55:35 -0300

    Fix service worker

--- public/offline.js
+++ public/offline.js
@@ -1,66 +1,60 @@
 /* eslint-disable no-restricted-globals */
-const CACHE_NAME = 'news-app-cache';
-var FILES_TO_CACHE = [
-    'index.html',
-    'static/js/bundle.js'
-];
+const CACHE = 'news-app-cache';
 
 self.addEventListener('install', (evt) => {
     console.log('[ServiceWorker] Install');
-    evt.waitUntil(
-        caches.open(CACHE_NAME).then((cache) => {
-          console.log('[ServiceWorker] Pre-caching offline page');
-          return cache.addAll(FILES_TO_CACHE);
-        })
-    );
-  
-    self.skipWaiting();
-  });
-  
-  self.addEventListener('activate', (evt) => {
-    console.log('[ServiceWorker] Activate');
-    // CODELAB: Remove previous cached data from disk.
-    evt.waitUntil(
-        caches.keys().then((keyList) => {
-          return Promise.all(keyList.map((key) => {
-            if (key !== CACHE_NAME) {
-              console.log('[ServiceWorker] Removing old cache', key);
-              return caches.delete(key);
-            }
-          }));
-        })
-    );
-  
-    self.clients.claim();
-  });
-  
-  self.addEventListener('fetch', (evt) => {
-    console.log('[ServiceWorker] Fetch', evt.request.url);
+    evt.waitUntil(precache());
+});
+
+
+addEventListener('fetch', (evt) => {
+    console.log('The service worker is serving the asset.');
     evt.respondWith(
-        caches.match(evt.request)
-            .then(function(response){
-                if (response) {
-                    return response;
-                }
-
-                const fetchRequest = evt.request.clone();
-
-                return fetch(fetchRequest).then(
-                    function(response) {
-                        if (!response || response.status !== 200 || response.type !== 'basic') {
-                            return response;
-                        }
-                        
-                        const responseToCache = response.clone();
-
-                        caches.open(CACHE_NAME)
-                            .then(function(cache) {
-                                cache.put(evt.request, responseToCache);
-                            });
-                        
-                        return response;
-                    }
-                );
-            })
+      caches.match(evt.request)
+        .then(function(response) {
+          if (response) {
+            return response;     // if valid response is found in cache return it
+          } else {
+            return fetch(evt.request)     //fetch from internet
+              .then(function(res) {
+                return caches.open(CACHE)
+                  .then(function(cache) {
+                    cache.put(evt.request.url, res.clone());    //save the response for future
+                    return res;   // return the fetched data
+                  })
+              })
+              .catch(function(err) {       // fallback mechanism
+                console.log(err);
+              });
+          }
+        })
     );
+});
+
+function fromCache(request) {
+  console.log("From cache");
+  return caches.open(CACHE).then(function(cache) {
+    return cache.match(request).then(function (matching) {
+      return matching || Promise.reject('no-match');
+    });
   });
+}
+
+function update(request) {
+  console.log("Updating cache");
+  return caches.open(CACHE).then(function (cache) {
+    return fetch(request).then(function (response) {
+      return cache.put(request, response);
+    });
+  });
+}
+  
+function precache() {
+  return caches.open(CACHE).then(function(cache) {
+    return cache.addAll([
+      `/`,
+      `/index.html`,
+      `/manifest.json`
+    ])
+  });
+}
--- src/index.js
+++ src/index.js
@@ -3,5 +3,6 @@ import './index.css';
 import { render } from 'react-dom';
 import Router from './components/Router';
 import 'bootstrap/dist/css/bootstrap.css';
+import * as serviceWorker from './serviceWorker';
 
 render(<Router />, document.getElementById('root'));
