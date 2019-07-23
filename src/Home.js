f6a511cdbfc7dce44502ffa615c6f6bf6ff60cfe
JOAQUIN LEONELLI <jleonelli@ar0fvfwm184hv2j.ml.com>
2019-07-22 23:29:30 -0300

    Se elimina localStorage del home component

--- src/Home.js
+++ src/Home.js
@@ -10,47 +10,22 @@ class Home extends Component {
 }
 
 componentDidMount() {
-    const localStorageRef = localStorage.getItem("headlines");
-    if (localStorageRef) {
-        setTimeout(() => {
-            this.setState({
-                headlines: JSON.parse(localStorageRef)
-            });
-        }, 1000);
-    } else {
-        var url = 'https://newsapi.org/v2/top-headlines?' +
+    var url = 'https://newsapi.org/v2/top-headlines?' +
         'country=us&' +
         'apiKey=9c08cbd8ac004bb08d92315c5bfb5fe6';
         var req = new Request(url);
         fetch(req).then(response => {
           response.json().then(resp => {
-              const a = resp
               this.setState({
-                  headlines: a.articles
+                  headlines: resp.articles
               });
          });
       });
-    }
-}
-
-componentDidUpdate() {
-    localStorage.setItem(
-        "headlines", 
-        JSON.stringify(this.state.headlines)
-    );
 }
 
 handleClick(e) {
     e.preventDefault();
     window.location.href = `/${e.target.name}`;
-}
-
-renderHeadlines() {
-    let rows = [];
-    this.state.headlines.forEach(a => {
-        rows.push(<Article article={a}/>);
-    })
-    return <tbody className="category-content">{rows}</tbody>;
 }
 
 render() {
